61ec2078c5b5d0306399e5ca62972139ead6aee9
juhayerayaz <aiazj97@gmail.com>
2022-03-03 17:35:17 +0600

    clear field added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const API_KEY = `703009c884855ab9713697b24a29a522`
 const searchTemperature = () => {
-    const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const searchField = document.getElementById('city-name');
+    searchText = searchField.value;
+    searchField.value = ''
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperature(data));
