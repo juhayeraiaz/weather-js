447efd19c0eff7e1f9397d77d9474655fbb24687
juhayerayaz <aiazj97@gmail.com>
2022-03-03 17:22:09 +0600

    push

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const API_KEY = `703009c884855ab9713697b24a29a522`
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('weather', temperature.weather[0].main);
+    // set weather icon 
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const img = document.getElementById('weather-icon');
+    img.setAttribute('src', url);
+}
